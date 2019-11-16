4df7fc8642149974d976a71d86b026f8ec250759
VioletaLibertad <morada.libertad@gmail.com>
2019-11-15 22:45:27 -0300

    first commit with django

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/violetalibertad/Documents/Doorlist/test/python_sandbox/sandbox/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
