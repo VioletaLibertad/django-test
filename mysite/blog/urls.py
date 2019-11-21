829e8ede9e08ec69d6852d589ccac7a3c6f7c54b
VioletaLibertad <morada.libertad@gmail.com>
2019-11-21 11:09:13 -0300

    style changes. bootstrap cdn updated

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
   path('post/<int:pk>/', views.post_detail, name='post_detail'),
   path('post/new', views.post_new, name='post_new'),
   path('post/<int:pk>/edit', views.post_edit, name='post_edit'),
-  path('results', retrieve_form_questions, name='retrieve_form_questions'),
+  path('results', retrieve_form_questions, name='results'),
 ]
